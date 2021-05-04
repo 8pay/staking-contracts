f1f8c14bda9e03cc766398c2022c1e7a736f3e25
AleG94 <alegen.dev@gmail.com>
2021-05-04 18:15:49 +0200

    Initial commit

---
+++ hardhat.config.js
@@ -0,0 +1,43 @@
+require('@nomiclabs/hardhat-ethers');
+require('@nomiclabs/hardhat-truffle5');
+require('@nomiclabs/hardhat-web3');
+require('hardhat-deploy');
+require('hardhat-contract-sizer');
+require('hardhat-abi-exporter');
+require('solidity-coverage');
+require('dotenv').config();
+
+/**
+ * @type import('hardhat/config').HardhatUserConfig
+ */
+module.exports = {
+  solidity: {
+    version: '0.8.4',
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200
+      }
+    }
+  },
+  networks: {
+    mainnet: {
+      url: 'https://bsc-dataseed.binance.org/',
+      accounts: { mnemonic: process.env.MNEMONIC }
+    },
+    testnet: {
+      url: 'https://data-seed-prebsc-1-s1.binance.org:8545',
+      accounts: { mnemonic: process.env.MNEMONIC }
+    }
+  },
+  namedAccounts: {
+    eightPayToken: {
+      default: 1,
+      testnet: '0xbcfee3c6de134d2db1fa7341615cab8ab082196e'
+    },
+    lpToken: {
+      default: 2,
+      testnet: '0x6073fd5461e09fc874a0880daf7e174d25228892'
+    }
+  }
+};
---
+++ scripts/deploy-lp-staking.js
@@ -0,0 +1,28 @@
+const { ethers, getNamedAccounts } = require('hardhat');
+
+async function main () {
+  const { eightPayToken, lpToken } = await getNamedAccounts();
+
+  const START_BLOCK = 120;
+  const END_BLOCK = 220;
+  const REWARD_PER_BLOCK = 2000;
+
+  const StakePool = await ethers.getContractFactory('StakePool');
+
+  const stakePool = await StakePool.deploy(
+    lpToken,
+    eightPayToken,
+    START_BLOCK,
+    END_BLOCK,
+    REWARD_PER_BLOCK
+  );
+
+  console.log(`StakePool: ${stakePool.address}`);
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch(error => {
+    console.error(error);
+    process.exit(1);
+  });
